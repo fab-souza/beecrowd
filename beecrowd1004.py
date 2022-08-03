a6a3eba6329a4da501f5557d707ace884b8a5346
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-08-03 17:25:10 -0300

    1004 em Python 3

---
+++ beecrowd1004.py
@@ -0,0 +1,13 @@
+# O problema pedia para ler dois números, efetuar sua multiplicação e apresentar o resultado
+
+a = int(input())
+b = int(input())
+prod = a * b
+print('PROD = {}'.format(prod))
+
+# Novamente, eu não mudaria muita coisa:
+
+a = int(input('Digite um número:'))
+b = int(input('Digite outro número: '))
+prod = a * b
+print('O produto entre os dois valores digitados é = {}'.format(prod))
