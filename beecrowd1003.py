00ce61a6fbcc8a62bd4280b134ad2eff8139bfc3
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-08-03 17:16:42 -0300

    1003 em Python 3

---
+++ beecrowd1003.py
@@ -0,0 +1,14 @@
+# O problema pedia para ler dois números, efetuar sua soma e apresentar o resultado.
+
+A = int(input())
+B = int(input())
+soma = A + B
+print('SOMA = {}'.format(soma))
+
+# Como eu faria:
+
+A = int(input('Digite um número: '))
+B = int(input('Digite o segundo número: '))
+soma = A + B
+print('A soma deles é:')
+print('SOMA = {}'.format(soma))
