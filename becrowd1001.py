46b9f4a7f209479deac9b14bcb4f08ebe43651e3
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-08-02 21:30:37 -0300

    1001 em Python 3

---
+++ becrowd1001.py
@@ -0,0 +1,16 @@
+# O problema é bem simples, pedia a entrada de dois números e a apresentação da soma. O código que a plataforma aceitou foi a seguinte:
+
+a = int(input())
+b = int(input())
+x = a + b
+print('X = {}'.format(x))
+
+# Como eu fiz pela primeira vez:
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite o segundo número: ')
+a = int(numero_1)
+b = int(numero_2)
+x = a + b
+print('A soma deles é:')
+print('X = {}'.format(x))
