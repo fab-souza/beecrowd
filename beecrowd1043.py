93673bc9fd2d7b2881921cf13a2df426d47bea29
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-09-08 22:41:12 -0300

    Not yet

---
+++ beecrowd1043.py
@@ -0,0 +1,7 @@
+# Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
+
+# Perimetro = XX.X
+
+# Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
+
+# Area = XX.X
