4dad8df7a07411cf03105f45a63366f22183ec79
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-10-17 19:17:49 -0300

    1043 em Python 3

--- beecrowd1043.py
+++ beecrowd1043.py
@@ -5,3 +5,13 @@
 # Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
 
 # Area = XX.X
+
+A, B, C = map(float, input().split()) 
+
+if (A < B+C and B < A+C and C < A+B):
+  perimetro = A+B+C
+  print('Perimetro = {:.1f}'.format(perimetro))
+
+else:
+  area = ((A+B)*C)/2
+  print('Area = {:.1f}'.format(area))
