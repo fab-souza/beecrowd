618ccc8cfd5ca151b387a38e5c118946253e36fb
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-10-26 22:44:02 -0300

    1049 em Python3

--- beecrowd1049.py
+++ beecrowd1049.py
@@ -3,3 +3,38 @@
 # Entrada
 # A entrada contém 3 palavras, uma em cada linha, necessárias para identificar o animal segundo a figura acima, com todas as letras minúsculas.
 
+estrutura1 = input()
+estrutura2 = input()
+estrutura3 = input()
+
+
+if estrutura1 == 'vertebrado':
+  # ave ou mamífero
+  if estrutura2 == 'ave':
+    # carnívoro ou onivoro
+    if estrutura3 == 'carnivoro':
+      print('aguia')
+    else:
+      print('pomba')
+  else: #mamífero
+    if estrutura3 == 'onivoro':
+      print('homem')
+    else: #herbivoro
+      print('vaca')
+  
+  
+else: #invertebrado
+  # inseto ou anelídeo
+  if estrutura2 == 'inseto':
+    # hematofago ou herbivoro
+    if estrutura3 == 'hematofago':
+      print('pulga')
+    else: #herbivoro
+      print('lagarta')
+  
+  else: #anelídeo
+    # hematofago ou onívoro
+    if estrutura3 == 'hematofago':
+      print('sanguessuga')
+    else: #onívoro
+      print('minhoca')
