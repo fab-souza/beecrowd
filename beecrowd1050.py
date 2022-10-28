9ec1b44d5a740cc7266a9400abd0db3d6f5316d3
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-10-27 21:47:44 -0300

    not yet

---
+++ beecrowd1050.py
@@ -0,0 +1,13 @@
+# Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+
+#DDD      Destination:
+#61        Brasilia
+#71        Salvador
+#11        São Paulo
+#21        Rio de Janeiro
+#32        Juiz de Fora
+#19        Campinas
+#27        Vitoria
+#31        Belo Horizonte
+
+# Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar: DDD nao cadastrado
