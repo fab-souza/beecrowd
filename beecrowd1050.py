6b7cbbe6c51306008a49411519eb74244fc388b9
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-10-29 13:44:02 -0300

    1050 em Python 3

--- beecrowd1050.py
+++ beecrowd1050.py
@@ -11,3 +11,32 @@
 #31        Belo Horizonte
 
 # Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar: DDD nao cadastrado
+
+ddd = input()
+
+if ddd == '61':
+  print('Brasilia')
+
+elif ddd == '71':
+  print('Salvador')
+  
+elif ddd == '11':
+  print('Sao Paulo')
+  
+elif ddd == '21':
+  print('Rio de Janeiro')
+  
+elif ddd == '32':
+  print('Juiz de Fora')
+  
+elif ddd == '19':
+  print('Campinas')
+  
+elif ddd == '27':
+  print('Vitoria')
+
+elif ddd == '31':
+  print('Belo Horizonte')
+  
+else: 
+  print('DDD nao cadastrado')
