f1944b8c0dff7596b5df69a84a827c8f6ce0c7f5
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-11-16 21:35:27 -0300

    Site não corrige...

--- beecrowd1066.py
+++ beecrowd1066.py
@@ -7,3 +7,42 @@ O arquivo de entrada contém 5 valores inteiros quaisquer.
 Saída
 Imprima a mensagem conforme o exemplo fornecido, uma mensagem por linha, não esquecendo o final de linha após cada uma.
 """
+
+valor1 = float(input())
+valor2 = float(input())
+valor3 = float(input())
+valor4 = float(input())
+valor5 = float(input())
+
+par = 0
+impar = 0
+positivo = 0
+negativo = 0
+
+valores = [valor1, valor2, valor3, valor4, valor5]
+
+for valor in valores:
+  if (valor % 2 == 0):
+    par = par + 1
+    if (valor > 0):
+      positivo += 1
+      
+    elif (valor < 0):
+      negativo += 1
+      
+    else:
+      continue
+
+  elif (valor % 2 != 0):
+    impar += 1
+    if (valor > 0):
+      positivo += 1
+
+    elif (valor < 0):
+      negativo += 1
+      
+    else:
+      continue
+
+  else:
+    continue
