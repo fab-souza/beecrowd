2232c2239196c9392539aff25372df09854d00fc
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-11-24 18:59:49 -0300

    RUNTIME ERROR

--- beecrowd1071.py
+++ beecrowd1071.py
@@ -9,3 +9,29 @@ Saída
 O programa deve imprimir um valor inteiro. Este valor é a soma dos valores ímpares que estão entre os valores fornecidos na entrada que deverá caber em um inteiro.
 
 """
+
+valor1 = int(input())
+valor2 = int(input())
+
+contador = 0
+
+if (valor1 > valor2):
+  if (valor2 % 2 != 0):
+    valor2 +=1
+  if (valor1 % 2 == 0):
+    valor1 +=1
+  for i in range(valor2, valor1):
+    if (i % 2 != 0):
+      contador = contador + i  
+
+elif (valor1 < valor2):
+  if (valor1 % 2 != 0):
+    valor1 +=1
+  for i in range(valor1, valor2):
+    if (i % 2 != 0):
+      contador = contador + i
+
+else:
+  continue
+
+print(contador)
