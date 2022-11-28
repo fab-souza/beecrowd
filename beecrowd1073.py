c32302f3145afb7e2fab77d1e25f0fd99fcc2d50
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-11-28 12:56:19 -0300

    1073 em Python 3

--- beecrowd1073.py
+++ beecrowd1073.py
@@ -11,3 +11,18 @@ Imprima o quadrado de cada um dos valores pares, de 1 até N, conforme o exemplo
 Tome cuidado! Algumas linguagens tem por padrão apresentarem como saída 1e+006 ao invés de 1000000 o que ocasionará resposta errada. Neste caso, configure a precisão adequadamente para que isso não ocorra.
 
 """
+
+entrada = int(input())
+
+if entrada % 2 == 0:
+  for valor in range (2, entrada+1, 2):
+    quadrado = valor ** 2
+    print('{}^2 = {}'.format(valor, quadrado))
+
+    
+else:
+  contador = 2
+  while contador < entrada:
+    quadrado = contador ** 2
+    print('{}^2 = {}'.format(contador, quadrado))
+    contador += 2
