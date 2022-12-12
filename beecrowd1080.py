0fb6f5847d0c7afcdb8dee2d85a6549d85e4e13b
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2022-12-12 13:25:03 -0300

    1080 em Python 3

--- beecrowd1080.py
+++ beecrowd1080.py
@@ -7,3 +7,19 @@ O arquivo de entrada contém 100 números inteiros, positivos e distintos.
 Saída
 Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
 """
+
+contador = 0
+entrada = 0
+posicao = 1
+dado = {'chave' : 0, 'valor' : 0}
+
+while contador < 100:
+  valor = int(input())
+  if valor > entrada:
+    entrada = valor
+    dado.update(chave = 'entrada', valor = 'posicao')
+  posicao += 1
+  contador += 1
+
+print(dado['chave'])
+print(dado['valor'])
