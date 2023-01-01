32a363b4989b1bf4fb753304b2b422e4f36f5dcf
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2023-01-01 15:09:17 -0300

    1096 em Python 3

--- beecrowd1096.py
+++ beecrowd1096.py
@@ -6,6 +6,17 @@ Não há nenhuma entrada neste problema.
 
 Saída
 Imprima a sequencia conforme exemplo abaixo
+
+I=1 J=7
+I=1 J=6
+I=1 J=5
+I=3 J=7
+I=3 J=6
+I=3 J=5
+...
+I=9 J=7
+I=9 J=6
+I=9 J=5
 """
 
 I = 1
