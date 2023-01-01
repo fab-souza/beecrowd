48d85455b295d6c000a0348a9b15652ba7b8f935
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2023-01-01 15:08:13 -0300

    1097 em Python 3

---
+++ beecrowd1097.py
@@ -0,0 +1,34 @@
+"""
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo.
+
+I=1 J=7
+I=1 J=6
+I=1 J=5
+I=3 J=9
+I=3 J=8
+I=3 J=7
+...
+I=9 J=15
+I=9 J=14
+I=9 J=13
+"""
+
+I = 1
+J = 7
+contador = 0
+
+while I <= 9:
+  print('I={} J={}'.format(I, J))
+  I += 0
+  J -= 1
+  contador += 1
+  if contador == 3:
+    I += 2
+    J += 5
+    contador = 0
