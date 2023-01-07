3658682acc9da4690b94608b05098460d7165247
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2023-01-07 09:55:53 -0300

    1114 em Python 3

--- beecrowd1114.py
+++ beecrowd1114.py
@@ -16,3 +16,31 @@ Exemplo:
 2002                  Acesso Permitido
 
 """
+
+contador = 0
+senha = 2002
+
+while (contador < 1000):
+  tentativa = int(input())
+  if (senha == tentativa):
+    print('Acesso Permitido')
+    break
+
+  else:
+    print('Senha Invalida')
+    contador += 1
+    
+ # Minha versão:
+
+contador = 0
+senha = 2002
+
+while (contador < 1000):
+  tentativa = int(input('Digite a senha: '))
+  if (senha == tentativa):
+    print('Acesso Permitido')
+    break
+
+  else:
+    print('Senha Invalida')
+    contador += 1
