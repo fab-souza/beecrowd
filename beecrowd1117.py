259857d6384e3b17cf875276ed63dc95da39637d
Fab de Souza <67301805+fab-souza@users.noreply.github.com>
2023-01-11 13:51:00 -0300

    1117 em Python 3

--- beecrowd1117.py
+++ beecrowd1117.py
@@ -17,3 +17,19 @@ Exemplo
 10.0       
 
 """
+
+contador = 0
+soma = 0
+
+while (contador < 2):
+  nota = float(input())
+  
+  if (nota < 0 or nota > 10):
+    print('nota invalida')
+    
+  else:
+    soma += nota
+    contador += 1
+
+media = soma / 2
+print('media = {}'.format(media))
